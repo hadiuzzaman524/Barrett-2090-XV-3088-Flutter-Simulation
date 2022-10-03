bb944dd4a8faf92b0d593754f88358b9ccf195ce
hadi.heavytask <hadi@heavytask.com>
2022-10-03 16:45:55 +0600

    feat: add images

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -4,15 +4,43 @@ class Assets {
 
   static const String imagesBarret = 'images/barret.jpg';
   static const String imagesBarret1 = 'images/barret1.jpg';
+  static const String imagesCall = 'images/call.png';
+  static const String imagesChan = 'images/chan.png';
+  static const String imagesClear = 'images/clear.PNG';
   static const String imagesComment = 'images/comment.png';
+  static const String imagesDanger = 'images/danger.png';
   static const String imagesDelete = 'images/delete.png';
+  static const String imagesEight = 'images/eight.png';
   static const String imagesEmail = 'images/email.png';
+  static const String imagesEnter = 'images/enter.png';
+  static const String imagesExch = 'images/exch.PNG';
+  static const String imagesFive = 'images/five.png';
   static const String imagesFlutter = 'images/flutter.png';
+  static const String imagesFour = 'images/four.png';
+  static const String imagesFrontpage = 'images/frontpage.png';
+  static const String imagesLogo = 'images/logo.png';
+  static const String imagesLower = 'images/lower.png';
+  static const String imagesLside = 'images/lside.png';
+  static const String imagesMenu = 'images/menu.PNG';
+  static const String imagesNine = 'images/nine.png';
+  static const String imagesOne = 'images/one.PNG';
+  static const String imagesProg = 'images/prog.PNG';
+  static const String imagesRadio = 'images/radio.PNG';
+  static const String imagesRside = 'images/rside.png';
   static const String imagesSell = 'images/sell.png';
   static const String imagesSettings = 'images/settings.png';
+  static const String imagesSeven = 'images/seven.png';
   static const String imagesSimulate = 'images/simulate.png';
+  static const String imagesSix = 'images/six.png';
+  static const String imagesThree = 'images/three.PNG';
   static const String imagesTower = 'images/tower.png';
+  static const String imagesTune = 'images/tune.png';
   static const String imagesTutorial = 'images/tutorial.png';
+  static const String imagesTwo = 'images/two.PNG';
+  static const String imagesUpper = 'images/upper.png';
+  static const String imagesVolminus = 'images/volminus.png';
+  static const String imagesVolplus = 'images/volplus.png';
   static const String imagesXv3088 = 'images/xv3088.jpg';
+  static const String imagesZero = 'images/zero.png';
 
 }
