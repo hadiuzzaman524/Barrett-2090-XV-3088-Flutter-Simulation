1fce9271722e571bd279b6a4447a104aebf1099e
hadi.heavytask <hadi@heavytask.com>
2022-10-10 12:54:05 +0600

    feat: design identification pages

--- lib/presentation/feature/barret_setup/identification.dart
+++ lib/presentation/feature/barret_setup/identification.dart
@@ -6,8 +6,9 @@ import '../../cubits/barret_setup/state.dart';
 
 class Identification extends StatelessWidget {
   final int identificationIndex;
-  const Identification({Key? key,
-  required this.identificationIndex}) : super(key: key);
+
+  const Identification({Key? key, required this.identificationIndex})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,69 +17,35 @@ class Identification extends StatelessWidget {
       child: Column(
         children: [
           FittedBox(
-            child:  Text(
-                  "===== <Identification page $identificationIndex> =====",
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: Colors.black54,
-                  ),
-
+            child: Text(
+              "===== <Identification page ${identificationIndex+1}> =====",
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+                color: Colors.black54,
+              ),
             ),
           ),
           Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 8,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "ID",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 4),
-                        color: Colors.black54,
-                        child: const Text(
-                          "      1861",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 22,
-                            color: Colors.white70,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 4,),
-                  const Text(
-                    "Press enter for Address Book",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black54
-                    ),
-                  ),
-                  Spacer(),
-                  const Text(
-                    "Press Call to Continue",
-                    style: TextStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
-              )),
+            child: identificationIndex == 0
+                ? const FirstPage()
+                : identificationIndex == 1
+                    ? const SecondPage()
+                    : identificationIndex == 2
+                        ? const ThirdPage()
+                        : identificationIndex == 3
+                            ? const FourthPage()
+                            : identificationIndex == 4
+                                ? const FifthPage()
+                                : const SixthPage(),
+          ),
+          const Text(
+            "Press Clear to Return",
+            style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54),
+          ),
           const FittedBox(
             child: Text(
               "============================",
@@ -91,6 +58,170 @@ class Identification extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          "Txover Type: 2090",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "S/N: 209016484",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "Options: 1, 2, 3, 4, 5, 6, 7",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ],
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          "Software Version: 2.16",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "DSP Version: 2.14",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "Core Version: 1.21",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ],
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          "SelCall IDs",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "INT1: N/A    OEM1: N/A",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "INT2: N/A    OEM2: N/A",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ],
+    );
+  }
+}
+
+class FourthPage extends StatelessWidget {
+  const FourthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          "Battery Rx: 14.5",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "Battery Tx: 14.3",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "PA Temperature: 34'",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ],
+    );
+  }
+}
+
+class FifthPage extends StatelessWidget {
+  const FifthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          "GPS Coordinates",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "Lat: 23'49.317N",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+        Text(
+          "Long: 90'24.118E",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ],
+    );
+  }
+}
+
+class SixthPage extends StatelessWidget {
+  const SixthPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: const [
+        Text(
+          "Internal Modem: N/A",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black87),
+        ),
+      ],
     );
   }
 }
